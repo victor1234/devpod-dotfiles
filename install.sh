d6701f9c743e1965f8d68c5d3b95bfa7a6dec2c9
Kataev Victor <kataev.victor.1234@gmail.com>
2025-07-15 18:20:50 +0200

    fix: git config

--- install.sh
+++ install.sh
@@ -4,3 +4,8 @@ set -e
 
 # Bash aliases
 echo "alias e='exit'" >>~/.bash_aliases
+
+# Setup git
+git config --global aliases.st status
+git config --global aliases.co commit
+git config --global aliases.ch checkout
