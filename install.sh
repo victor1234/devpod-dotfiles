47428cc39c10141cec9aaa4fd19c2bb19b2fe78d
Kataev Victor <kataev.victor.1234@gmail.com>
2025-07-15 18:40:59 +0200

    fix: uv install

--- install.sh
+++ install.sh
@@ -12,6 +12,7 @@ git config --global alias.ch checkout
 
 # Install uv
 curl -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.local/bin/env
 
 # Install pre-commit
 uv tool install pre-commit
